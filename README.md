54e635f49be75f82447b4c27d097f96c2339ada1
vova <vova.omelenchuk@gmail.com>
2022-03-13 21:57:45 +0200

    Create the project

---
+++ README.md
@@ -0,0 +1,17 @@
+# TelegramReport
+
+1. Get Telegram dev credential from https://my.telegram.org/auth?to=apps
+2. Add api_id, api_hash and set you phone number and username in config.ini
+3. Setup python 3.8 - https://www.python.org/downloads/release/python-380/
+4. Install Telethon: pip3 install telethon
+5. Add target account yo want to report in target_accounts.txt 
+6. Run ./report_accounts.py, enter auth password
+
+------------------------------------------------------------------------------
+
+1. Отримати девелоперські креденшили https://my.telegram.org/auth?to=apps
+2. Додайте api_id, api_hash і ваш номер телефону та ім'я в config.ini
+3. Встановіть python 3.8 - https://www.python.org/downloads/release/python-380/
+4. Встановіть Telethon: pip3 install telethon
+5. Додайте назви акаунтів, на які ви маєте намір поскаржитись в файлі target_accounts.txt 
+6. Запустіть ./report_accounts.py, аутентифікуйтесь кодом
---
+++ report_accounts.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import configparser
+import re
+
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon import functions, types
+from telethon import errors
+
+import random
+import time
+
+ACCOUNTS = set(map(lambda x: x.replace('\n', ''), list(open("target_accounts.txt"))))
+
+# Enter messages for report
+MESSAGES = [
+    '',
+    '',
+    '',
+    '',
+    ''
+]
+REASONS = [
+    types.InputReportReasonViolence(),
+    types.InputReportReasonSpam(),
+    types.InputReportReasonOther(),
+    types.InputReportReasonFake(),
+]
+INDEXES = [ random.randrange(0, len(ACCOUNTS)) for _ in range(len(ACCOUNTS)) ]
+
+FLOOD1 = []
+FLOOD2 = []
+SLEEP_SECONDS1 = 0
+SLEEP_SECONDS2 = 0
+
+# BEGIN ##############################################################################
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+api_id = config['Telegram']['api_id']
+api_hash = config['Telegram']['api_hash']
+
+api_hash = str(api_hash)
+
+phone = config['Telegram']['phone']
+username = config['Telegram']['username']
+
+# Create the client and connect
+client = TelegramClient(username, api_id, api_hash)
+
+async def main(phone):
+    await client.start()
+    print("Client Created")
+
+    # Ensure you're authorized
+    if await client.is_user_authorized() == False:
+        await client.send_code_request(phone)
+        try:
+            await client.sign_in(phone, input('Enter the code: '))
+        except SessionPasswordNeededError:
+            await client.sign_in(password=input('Password: '))
+
+    while True:
+        for i in INDEXES:
+            try:
+                result = await client(functions.messages.ReportRequest(
+                    peer=ACCOUNTS[i],
+                    id=[42],
+                    reason=REASONS[random.randrange(0, len(REASONS))],
+                    message=MESSAGES[random.randrange(0, len(MESSAGES))],
+                ))
+                print(f'ACCOUNT: {ACCOUNTS[i]}: Reported - {result}')
+
+            except errors.rpcerrorlist.FloodWaitError as flood_error:
+                print(f'ACCOUNT: {ACCOUNTS[i]}: Flood wait error, adding to FLOOD list')
+                FLOOD1.append(ACCOUNTS[i]) # resolve for later 
+                SLEEP_SECONDS1 = int(re.search(r'\d+', flood_error.__repr__()).group())
+
+            except ValueError as value_error:
+                print(f'ACCOUNT: {ACCOUNTS[i]}: Unreachable: {value_error}')
+
+            except Exception as exception:
+                print(f'ACCOUNT: {ACCOUNTS[i]}: Unhandled exception {exception} with type {type(exception)}. Please fix the code.')
+
+        while len(FLOOD1) != 0 or len(FLOOD2) != 0:
+            print(f'Sleep: {SLEEP_SECONDS1}')
+            time.sleep(SLEEP_SECONDS1)
+
+            for account in FLOOD1:
+                try:
+                    result = await client(functions.messages.ReportRequest(
+                        peer=account,
+                        id=[42],
+                        reason=REASONS[random.randrange(0, len(REASONS))],
+                        message=MESSAGES[random.randrange(0, len(MESSAGES))],
+                    ))
+                    print(f'ACCOUNT: {account}: Reported - {result}')
+
+                except errors.rpcerrorlist.FloodWaitError as flood_error:
+                    print(f'ACCOUNT: {account}: Flood wait error, adding to FLOOD list')
+                    FLOOD2.append(account) # resolve for later 
+                    SLEEP_SECONDS2 = int(re.search(r'\d+', flood_error.__repr__()).group())
+
+                except ValueError as value_error:
+                    print(f'ACCOUNT: {account}: Unreachable: {value_error}')
+
+                except Exception as exception:
+                    print(f'ACCOUNT: {account}: Unhandled exception {exception} with type {type(exception)}. Please fix the code.')      
+            FLOOD1.clear()
+
+            print(f'Sleep: {SLEEP_SECONDS2}')
+            time.sleep(SLEEP_SECONDS2)
+
+            for account in FLOOD2:
+                try:
+                    result = await client(functions.messages.ReportRequest(
+                        peer=account,
+                        id=[42],
+                        reason=REASONS[random.randrange(0, len(REASONS))],
+                        message=MESSAGES[random.randrange(0, len(MESSAGES))],
+                    ))
+                    print(f'ACCOUNT: {account}: Reported - {result}')
+
+                except errors.rpcerrorlist.FloodWaitError as flood_error:
+                    print(f'ACCOUNT: {account}: Flood wait error, adding to FLOOD list')
+                    FLOOD1.append(account) # resolve for later 
+                    SLEEP_SECONDS1 = int(re.search(r'\d+', flood_error.__repr__()).group())
+
+                except ValueError as value_error:
+                    print(f'ACCOUNT: {account}: Unreachable: {value_error}')
+
+                except Exception as exception:
+                    print(f'ACCOUNT: {account}: Unhandled exception {exception} with type {type(exception)}. Please fix the code.')      
+            FLOOD2.clear()
+
+        break # white true break
+
+with client:
+    client.loop.run_until_complete(main(phone))
